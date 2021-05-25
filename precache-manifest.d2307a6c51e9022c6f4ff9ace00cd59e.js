c4340fd919518c2b167c0bc949427119473c46c2
Oleksandr Orel <oleksandrorel@ukr.net>
2021-05-25 13:20:41 +0300

    Updates

---
+++ precache-manifest.d2307a6c51e9022c6f4ff9ace00cd59e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c75432b5be8efe733242aebdd047fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6cf12d3b9a8ec6fe9867",
+    "url": "./static/css/main.7dc47e92.chunk.css"
+  },
+  {
+    "revision": "f4910a8ee5c7ba5d5eed",
+    "url": "./static/js/2.4d2fb0a4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.4d2fb0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf12d3b9a8ec6fe9867",
+    "url": "./static/js/main.7e5a53ba.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.d2307a6c51e9022c6f4ff9ace00cd59e.js"
 );
 
 self.addEventListener('message', (event) => {
